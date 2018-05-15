9d72cb6dac68db581ba1bb73d327cc6f53bfedff
uNick51 <nick51@hotmail.fr>
2018-05-15 20:33:51 +0200

    fait

--- exercice5.php
+++ exercice5.php
@@ -6,7 +6,7 @@
 <body>
 
 <?php
-$texte = "quelque chose";
+$texte = "";
 echo "on a ".$texte;
 ?>
 
--- index.php
+++ index.php
@@ -6,46 +6,47 @@
 <body>
 
   <?php
+
   // ---------------1---------
   $nom = "fievet";
-  echo "$nom";
+  echo "<br />$nom";
 
   // ---------------2---------
   $nom = "fievet";
   $prenom = "nicolas";
   $age = "38";
-  echo "$nom"." $prenom"." $age";
+  echo "<br />"."$nom"." $prenom"." $age";
 
   // ---------------3---------
   $km = 125;
-  echo $km;
+  echo "<br />".$km;
 
   // ---------------4---------
   $texte = "string";
   $entier = 38;
   $decimal = 12.34;
   $debutant = true;
-  echo "$texte"." $entier"." $decimal"." $debutant";
+  echo "<br />".$texte." $entier"." $decimal"." $debutant";
 
   // ---------------5---------
-  $texte = "quelque chose";
-  echo "on a ".$texte;
+  $texte = "";
+  echo "<br />"."on a ".$texte;
 
   // ---------------6---------
   $nom = "nicolas";
-  echo "Bonjour ".$nom.", comment va tu ?";
+  echo "<br />"."Bonjour ".$nom.", comment va tu ?";
 
   // ---------------7---------
   $nom = "fievet";
   $prenom = "nicolas";
   $age = 38;
-  echo "Bonjour ".$nom." $prenom".",tu as ".$age." ans.";
+  echo "<br />"."Bonjour ".$nom." $prenom".",tu as ".$age." ans.";
 
   // ---------------8---------
   $op1 = 3+4;
   $op2 = 5*20;
   $op3 = 45/5;
-  echo $op1." $op2 ".$op3;
+  echo "<br />".$op1." $op2 ".$op3;
   ?>
 
 </body>
